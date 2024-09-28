e7451e3fedaa64168c7f3c3f233310cf41aed005
Peter Molnar <pm@aston.sk>
2024-09-28 17:22:04 +0200

    init

---
+++ bin/api_flow_sidecar.dart
@@ -0,0 +1,5 @@
+import 'package:api_flow_sidecar/api_flow_sidecar.dart' as api_flow_sidecar;
+
+void main(List<String> arguments) {
+  print('Hello world: ${api_flow_sidecar.calculate()}!');
+}
---
+++ test/api_flow_sidecar_test.dart
@@ -0,0 +1,8 @@
+import 'package:api_flow_sidecar/api_flow_sidecar.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
